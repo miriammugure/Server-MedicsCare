171feb14010faff00b91722278925e591dbcb426
miriam <miriammugure610@gmail.com>
2024-07-04 14:33:38 +0300

    add the database and the configured server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import usersRouter from './routes/user.routes.js';
+import cors from 'cors'
+const app = express();
+
+
+app.use(cors({
+   origin: 'http://localhost:5173',
+
+   methods: 'GET,PUT,PATCH,POST,DELETE',
+}))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use('/api/users', usersRouter);
+
+app.listen(3000, () => {
+  console.log('App is listening on port 3000...');
+});
+
+
