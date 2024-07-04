0bc1f8a1a82c8ccb75bcb0d0420cc6dcf8e8a931
miriam <miriammugure610@gmail.com>
2024-07-04 14:35:50 +0300

    add the users routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,34 +1,34 @@
-// import { Router } from 'express';
-// import { PrismaClient } from '@prisma/client';
-// import bcrypt from 'bcrypt';
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
-// const prisma = new PrismaClient();
-// const router = Router();
+const prisma = new PrismaClient();
+const router = Router();
 
-// router.post('/register', async (req, res) => {
-//   try {
-//     const { firstName, lastName, email, phoneNumber, password } = req.body;
-//     console.log("Received request to register user:", req.body); 
-//     const parsedPhoneNumber = parseInt(phoneNumber, 10);
-//     const hashedPassword = bcrypt.hashSync(password, 10);
+router.post('/register', async (req, res) => {
+  try {
+    const { firstName, lastName, email, phoneNumber, password } = req.body;
+    console.log("Received request to register user:", req.body); 
+    const parsedPhoneNumber = parseInt(phoneNumber, 10);
+    const hashedPassword = bcrypt.hashSync(password, 10);
    
 
-//     const newUser = await prisma.user.create({
-//       data: {
-//         firstName,
-//         lastName,
-//         email,
-//         phoneNumber:parsedPhoneNumber,
-//         password: hashedPassword,
-//       },
-//     });
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        phoneNumber:parsedPhoneNumber,
+        password: hashedPassword,
+      },
+    });
 
    
-//     res.status(201).json({ success: true, message: 'User registered successfully' });
-//   } catch (error) {
+    res.status(201).json({ success: true, message: 'User registered successfully' });
+  } catch (error) {
    
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// });
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
 
-// export default router;
+export default router;
